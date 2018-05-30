f474b3475c171877caa2f1d96cbcdd5c52102831
SPARTA-SYSTEMS\andrew.parise <andrew.parise@spartassystems.com>
2018-05-29 21:17:15 -0400

    Andrew Parise - added maven, refactored naming and project structure

---
+++ src/main/test/com/parisesoftware/datastructure/bst/BinarySearchTreeImplTest.java
@@ -0,0 +1,64 @@
+package com.parisesoftware.datastructure.bst;
+
+import com.parisesoftware.datastructure.linkedlist.LinkedListImpl;
+
+/*
+ * Andrew Parise
+ * March 10th 2016
+ * Assignment 6.1
+ * Programming II
+ * 
+ * BinarySearchTreeImplTest.java
+ * 
+ * Test Class to test the functionality of the BinarySearchTreeImpl.java methods
+ * 
+ */
+public class BinarySearchTreeImplTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinarySearchTreeImpl bst = new BinarySearchTreeImpl();
+		
+		//test isEmpty()
+		if(bst.isEmpty()){
+			System.out.println("The BST is empty");
+		}
+		
+		//test insert()
+		bst.insert("Boogie");
+		bst.insert("Dog");
+		bst.insert("Road");
+		bst.insert("Ocean");
+		bst.insert("Apart");
+		bst.insert("Begin");
+		bst.insert("Wings");
+		bst.insert("Haywire");
+		
+		if(!bst.isEmpty()){
+			System.out.println("The BST is not empty.");
+		}
+		
+		
+		//test traversal functions
+		bst.populateTraverseLists();
+		LinkedListImpl inorderTrav = bst.getInOrder();
+		LinkedListImpl preOrderTrav = bst.getPreOrder();
+		LinkedListImpl postOrderTrav = bst.getPostOrder();
+		
+	
+		
+		System.out.println("Inorder Traversal: " + inorderTrav.toString());
+		System.out.println("PreOrder Traversal: " + preOrderTrav.toString());
+		System.out.println("PostOrder Traversal: " + postOrderTrav.toString());
+		
+		//Test removal functions
+		bst.removeNode("Apart");
+		bst.removeNode("Ocean");
+		
+		
+		bst.populateTraverseLists();
+		
+		System.out.println("New List (post-order): " + bst.getPostOrder());
+	}
+
+}
